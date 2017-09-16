186167c5df00ff082ae3dd8bda35f13763bc62ee
MaksymKarpenko <karpenko.maxim87gmail.com>
2017-09-17 00:24:09 +0300

    workflow

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/, 
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader','sass-loader'],
+                    publicPath: '/dist'
+                })
+            },{
+            	test: /\.js$/,
+      			exclude: /node_modules/,
+      			use: 'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            template: './src/index.html' 
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            allChunks: true
+        })
+    ]
+}
+		
