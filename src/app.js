f65c981689c039e556621ab83cb851f230802f19
MaksymKarpenko <karpenko.maxim87gmail.com>
2017-09-17 15:09:58 +0300

    add reducers folder, inside this folder add booksReducers, cartReducers, index.js files

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
 "use strict"
 import {createStore} from 'redux';
+import reducers from './reducers/index'
 
-const reducer = function (state=0, action) {
-	switch(action.type){
-		case "INCREMENT":
-		return state + action.payload;
-		break;
-	}
-	return state
-}
-
-const store = createStore(reducer);
+const store = createStore(reducers);
 
 store.subscribe(function () {
-	console.log('current state is: ' + store.getState());
+	console.log('current state is: ', store.getState());
+	//console.log('current price is: ', store.getState()[1].price);
 })
 
-store.dispatch({type: "INCREMENT", payload: 1})
-store.dispatch({type: "INCREMENT", payload: 1})
-store.dispatch({type: "INCREMENT", payload: 1})
+store.dispatch({
+	type: "POST_BOOK", 
+	payload: [{
+			id: 1,
+			title: 'this is the book title',
+			description: 'this is the book description',
+			price: 33.33
+		},
+		{
+			id: 2,
+			title: 'this is the second book title',
+			description: 'this is the second book description',
+			price: 20
+		}
+	]
+})
+
+store.dispatch({
+	type: "DELETE_BOOK", 
+	payload: { id: 1}
+})
+
+store.dispatch({
+	type: "UPDATE_BOOK",
+	payload: {
+		id: 2,
+		title: 'Learn React in 24h'
+	}
+})
+
+store.dispatch({
+	type: "ADD_TO_CART",
+	payload: [{id: 2}]
+})
