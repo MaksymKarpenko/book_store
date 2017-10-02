ed3e9ea3410a39d33a2c6b17b9adc560d3c8f63a
MaksymKarpenko <karpenko.maxim87gmail.com>
2017-10-02 15:45:05 +0300

    add api in app.js, add get, post, update, delete method0

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ mongoose.connect('mongodb://localhost:27017/bookshop', {useMongoClient:true});
 
 var Books = require('./models/books.js');
 
-app.post('./books', function(req, res){
+//---->>> POST BOOKS <<<----
+app.post('/books', function(req, res){
 	var book = req.body;
 	Books.create(book, function(err, books){
 		if(err){
@@ -34,6 +35,52 @@ app.post('./books', function(req, res){
 		res.json(books);
 	})
 });
+
+//---->>>> GET BOOKS <<<----------------
+app.get('/books', function(req, res){
+  Books.find(function(err, books){
+    if(err){
+      throw err;
+    }
+      res.json(books);
+  })
+})
+
+//---->>>> DELETE BOOKS <<<------------
+app.delete('/books/:_id', function(req, res){
+  var query = {_id: req.params._id};
+
+  Books.remove(query, function(err, books){
+    if(err){
+      throw err;
+    }
+    res.json(books);
+  })
+});
+
+//---->>> UPDATE BOOKS <<<<-----------
+app.put('/books/:_id', function(req, res){
+  var book = req.body;
+  var query = req.params._id;
+  var update = {
+    '$set':{
+      title:book.title,
+      description:book.description,
+      image:book.image,
+      price:book.price
+    }
+  };
+
+    var options={new: true};
+
+    Books.findOneAndUpdate(query, update, options, function(err, books){
+      if(err){
+        throw err;
+      }
+        res.json(books);
+    })
+
+})
 
 app.get('*', function(req, res) {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
